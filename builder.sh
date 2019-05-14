f72169d392c4a7670119e50aa7a6dad26f1cc83f
wangxulong <duanxian0402@126.com>
2019-05-14 17:57:57 +0800

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,15 +1,3 @@
 #!/usr/bin/env bash
 
-REGISTRY=image.overlay2.lsn.im
-
-action=$1
-branch=$(git rev-parse --abbrev-ref HEAD)
-deploy_dir="dev"
- 
-if [[ ${action} == "deploy" ]]; then
-    echo "deploy"
-fi
-
-if [[ ${action} == "build" ]]; then
-    echo "执行我了，是build"
-fi
+ echo "123"
